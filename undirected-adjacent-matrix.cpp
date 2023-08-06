0761412cd1bdc08bc47630524409fa353ffdd0e9
anisul islam <anisulislam980340@gmail.com>
2023-08-06 21:30:36 +0600

    adjacent matrix weighted

--- undirected-adjacent-matrix.cpp
+++ undirected-adjacent-matrix.cpp
@@ -6,10 +6,10 @@ int main(){
     int n, m;
     cin>>n>>m;
     for(int i=0; i<m; i++){
-        int u, v;
-        cin>>u>>v;
-        adjmat[u][v]=1 ;//for directed graph
-        adjmat[v][u]=1 ; //for undirected graph
+        int u, v, w;
+        cin>>u>>v>>w;
+        adjmat[u][v]=w ;//for directed graph
+        adjmat[v][u]=w ; //for undirected graph
     }
     
     //check adjmat 
