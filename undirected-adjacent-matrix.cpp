73bb15cb82950196f0cb26268cd04caa4b3f99c9
anisul islam <anisulislam980340@gmail.com>
2023-08-06 21:21:31 +0600

    adjacent matrix comleted

---
+++ undirected-adjacent-matrix.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e3+5;
+int adjmat [N][N];
+int main(){
+    int n, m;
+    cin>>n>>m;
+    for(int i=0; i<m; i++){
+        int u, v;
+        cin>>u>>v;
+        adjmat[u][v]=1 ;//for directed graph
+        adjmat[v][u]=1 ; //for undirected graph
+    }
+    
+    //check adjmat 
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cout<<adjmat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
